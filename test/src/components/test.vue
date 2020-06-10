c9c1333422ddf6a33ea6f077abec2a0a8e4f07a9
ZhangSong <757910395@qq.com>
2020-06-10 17:26:11 +0800

    Initial commit

---
+++ test/src/components/test.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  2222222222222222
+  <p>3333333333</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'test'
+}
+</script>
+
+<style scoped>
+div {
+  font-size: 50px;
+  color: blueviolet;
+}
+</style>
